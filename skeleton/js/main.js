2ac1e6c0d6004fe83c6cde12f45db2ea10a08887
chrisandgigi <chrisandgigi@io.com>
2016-03-16 17:00:37 -0400

    Make a beautiful TicTacToe UI.

--- skeleton/js/main.js
+++ skeleton/js/main.js
@@ -2,7 +2,7 @@ var View = require('./ttt-view');
 var Game = require('../../ttt-core-solution/game');
 
 $(function () {
-  // Your code here
+  var $game1 = $(".ttt");
+  var game = new Game();
+  var view = new View(game, $game1);
 });
-
-
--- skeleton/js/ttt-view.js
+++ skeleton/js/ttt-view.js
@@ -1,13 +1,42 @@
 var View = function (game, $el) {
+  this.game = game;
+  this.$el = $el;
+  this.setupBoard();
+  this.bindEvents();
 };
 
 View.prototype.bindEvents = function () {
+  this.$el.on("click", "li", function(e) {
+    var $li = $(e.currentTarget);
+    this.makeMove($li);
+  }.bind(this));
 };
 
 View.prototype.makeMove = function ($square) {
+  var id = $square.attr("id");
+  var pos = [id % 3, Math.floor(id/3)];
+  if(!this.game.board.isEmptyPos(pos)){
+    alert("Not a valid move!");
+  } else {
+    var winner = this.game.currentPlayer;
+    $square.toggleClass(this.game.currentPlayer);
+    this.game.playMove(pos);
+    if(this.game.isOver() && this.game.winner()){
+      alert(winner + " has won!");
+    } else if (this.game.isOver()) {
+      alert("No moves left. It's a tie.");
+    }
+  }
 };
 
 View.prototype.setupBoard = function () {
+  var $figure = this.$el;
+  var html = "<ul class=\"grid group\">";
+  for (var i = 0; i < 9; i++) {
+    html += "<li id=\"" + i + "\"></li>";
+  }
+  html += "</ul>";
+  $figure.append(html);
 };
 
 module.exports = View;
